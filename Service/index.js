92afaad7d00ae5aa042ab3c418fe96fb64593f9e
ryanpetersonOF <ryan@openfin.co>
2019-03-21 15:16:02 +0000

    Convert to Channels

--- Service/index.js
+++ Service/index.js
@@ -1,7 +1,7 @@
 import pipe from "../Shared/pipe";
 
 export default async function connectStoreToProvider(store, actionHelper = a => a) {
-    const provider = await fin.desktop.Service.register()
+    const provider = await fin.desktop.InterApplicationBus.Channel.create("redux-example");
     provider.register('dispatch-action', pipe(actionHelper, store.dispatch.bind(store)))
     store.subscribe(() => provider.publish('state-change', store.getState()))
     provider.register('getState', () => store.getState())
